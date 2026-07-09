171b568f0d4d3f3f2952effc1e088346978ac16d
firdyridho <firdyridho9@gmail.com>
2026-07-09 07:45:09 +0700

    First Upload

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+// Pengaturan Database
+$host = "localhost"; // Di InfinityFree biasanya berbentuk "sqlxxx.epizy.com"
+$user = "root";      // Di InfinityFree biasanya "epiz_xxxxxxx"
+$pass = "";          // Password database InfinityFree kamu
+$db   = "hifzly_db"; // Nama database di InfinityFree
+
+// Membuat koneksi
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}

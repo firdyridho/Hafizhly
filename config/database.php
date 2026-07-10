a85726b9db0b558720e6ae8dd53c62c660451f62
firdyridho <firdyridho9@gmail.com>
2026-07-10 14:34:44 +0700

    login improve

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // Pengaturan Database
-$host = "sql206.infinityfree.com"; // Di InfinityFree biasanya berbentuk "sqlxxx.epizy.com"
-$user = "if0_42360028";      // Di InfinityFree biasanya "epiz_xxxxxxx"
-$pass = "ZEQ85K2AIzup";          // Password database InfinityFree kamu
-$db   = "if0_42360028_hifzly_db"; // Nama database di InfinityFree
+$host = "localhost"; // Di InfinityFree biasanya berbentuk "sqlxxx.epizy.com"
+$user = "root";      // Di InfinityFree biasanya "epiz_xxxxxxx"
+$pass = "";          // Password database InfinityFree kamu
+$db   = "hifzly_db"; // Nama database di InfinityFree
 
 // Membuat koneksi
 $conn = mysqli_connect($host, $user, $pass, $db);

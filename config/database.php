53d554eba390cca7187ab86db05a1d068e9c3495
firdyridho <firdyridho9@gmail.com>
2026-07-10 18:45:09 +0700

    database setup

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // Pengaturan Database
-$host = "sql206.infinityfree.com"; // Di InfinityFree biasanya berbentuk "sqlxxx.epizy.com"
-$user = "if0_42360028";      // Di InfinityFree biasanya "epiz_xxxxxxx"
-$pass = "ZEQ85K2AIzup";          // Password database InfinityFree kamu
-$db   = "if0_42360028_hifzly_db"; // Nama database di InfinityFree
+$host = "sql303.infinityfree.com"; // Di InfinityFree biasanya berbentuk "sqlxxx.epizy.com"
+$user = "if0_42360001";      // Di InfinityFree biasanya "epiz_xxxxxxx"
+$pass = "s2px6nqvwFFa";          // Password database InfinityFree kamu
+$db   = "if0_42360001_hafizhly_db"; // Nama database di InfinityFree
 
 // Membuat koneksi
 $conn = mysqli_connect($host, $user, $pass, $db);

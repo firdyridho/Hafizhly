96324a68a4b0834b3104b7b1b443f1189d1519b9
firdyridho <firdyridho9@gmail.com>
2026-07-10 19:02:15 +0700

    update footer

--- index.php
+++ index.php
@@ -1209,11 +1209,11 @@ if (isset($_SESSION['user_id'])) {
                 <span style="color: #cbd5e1;">|</span>
                 <a href="terms.php" style="color: var(--text-gray); text-decoration: none; font-size: 0.85rem;">Syarat & Ketentuan</a>
             </div>
-            <div class="footer-social">
+            <!-- <div class="footer-social">
                 <a href="#"><i class="fa-brands fa-instagram"></i></a>
                 <a href="#"><i class="fa-brands fa-tiktok"></i></a>
                 <a href="#"><i class="fa-brands fa-youtube"></i></a>
-            </div>
+            </div> -->
         </div>
     </footer>
 
